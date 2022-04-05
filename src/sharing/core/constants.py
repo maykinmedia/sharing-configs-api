09ab60b108c364c1162f7107ae8aee3724327d01
Anna Shamray <anna.shamray@maykinmedia.nl>
2022-04-05 18:16:58 +0200

    :sparkles: [https://github.com/maykinmedia/sharing-configs/issues/23] add RootPathConfig model

--- src/sharing/core/constants.py
+++ src/sharing/core/constants.py
@@ -6,3 +6,8 @@ from djchoices import ChoiceItem, DjangoChoices
 class ConfigTypes(DjangoChoices):
     github = ChoiceItem("github", _("Github"))
     debug = ChoiceItem("debug", _("Testing"))
+
+
+class PermissionModes(DjangoChoices):
+    read = ChoiceItem("read", _("Read-only"))
+    write = ChoiceItem("write", _("Read and write"))
